700a1081cca14038d9ab222e12bb4595b0cefbd1
Octogon69 <145037985+Octogon69@users.noreply.github.com>
2024-01-23 16:17:38 +0000

    issues fixed
    enemies now dissapear when shot

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
+
+    
     private void OnTriggerEnter2D(Collider2D collision)
     {
          if (collision.GetComponent<EnemyMovement>())
@@ -11,5 +13,9 @@ public class Bullet : MonoBehaviour
             Destroy(collision.gameObject);
             Destroy(gameObject);
         }
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Destroy(gameObject);
     }
 }
